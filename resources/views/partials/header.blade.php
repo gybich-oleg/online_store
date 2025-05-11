abdfe0a6c59a2586bb59db5960dd8493a8113a52
gybich-oleg <gybich.oleg68@gmail.com>
2025-05-11 19:18:37 +0300

    creat css header and input form search

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,13 @@
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 <header>
     <nav>
         <a href="{{ route('home') }}">Главная</a>
-        <a href="{{ route('product.search') }}">Поиск</a>
-        <a href="{{ route('cart.index') }}">Корзина</a>
+        <form action="{{ route('product.search') }}" method="GET">
+            <input type="text" name="query" placeholder="Поиск товаров...">
+            <button type="submit">Найти</button>
+        </form>
+{{--        <a href="{{ route('product.search') }}">Поиск</a>--}}
+        <a class="cart" href="{{ route('cart.index') }}">Корзина</a>
         {{-- Можно добавить ссылки на личный кабинет и регистрацию --}}
     </nav>
 </header>
